4e5f61c23e3530f9874cfa2936ee9c6663c47618
ElizabethOkerio <elizaokerio@gmail.com>
2022-09-21 11:01:31 +0300

    add sample project

---
+++ Microsoft.OData.AzureFunctionsSample/EdmModelProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.OData.AzureFunctions;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
+namespace Microsoft.OData.AzureFunctionsSample
+{
+    public class EdmModelProvider : IEdmModelProvider
+    {
+        public IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Customer>("Customers");
+            return builder.GetEdmModel();
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Postcode { get; set; }
+    }
+}
